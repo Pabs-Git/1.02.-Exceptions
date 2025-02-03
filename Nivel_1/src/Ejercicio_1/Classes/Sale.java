309e5ccd9b32c75a47bf8720794c5a8d92ce8310
Pablo Orbea <pabloorbea1@gmail.com>
2025-02-03 09:11:58 +0100

    Prepared for correction

---
+++ Nivel_1/src/Ejercicio_1/Classes/Sale.java
@@ -0,0 +1,42 @@
+package Ejercicio_1.Classes;
+
+import Ejercicio_1.Exceptions.EmptySaleException;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Sale {
+
+    private List<Product> products;
+    private double totalPrice;
+
+    public Sale() {
+        this.products = new ArrayList<>();
+        this.totalPrice = 0.0;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void calculateTotalPrice() throws EmptySaleException {
+
+        if (products.isEmpty()) {
+
+            throw new EmptySaleException("To make a sale you must first add products");
+        }
+
+        totalPrice = 0;
+
+        for (Product product : products) {
+
+            totalPrice += product.getPrice();
+        }
+
+        System.out.println("The total sale price is: " + totalPrice);
+    }
+}
---
+++ Nivel_1/src/Ejercicio_1/Exceptions/EmptySaleException.java
@@ -0,0 +1,9 @@
+package Ejercicio_1.Exceptions;
+
+public class EmptySaleException extends Exception {
+
+    public EmptySaleException (String message){
+
+        super(message);
+    }
+}
